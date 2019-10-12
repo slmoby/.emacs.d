9c69b3ea93ae0520a5c526ec8657d8492ae90f79
lotusdocker <lotusdocker@gmail.com>
2019-10-12 10:14:39 +0800

    Sat 12 Oct 2019 10:14:39 AM CST

--- init.el
+++ init.el
@@ -24,9 +24,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files nil)
  '(package-selected-packages
    (quote
-    (try auto-complete counsel swiper ace-window org-bullets use-package))))
+    (pdf-view-restore org-pdfview doom-modeline doom doom-themes eshell-autojump telega pyim-basedict pyim auctex-latexmk try auto-complete counsel swiper ace-window org-bullets use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
